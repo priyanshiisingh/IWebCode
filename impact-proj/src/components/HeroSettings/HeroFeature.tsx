3ceb96eb893f4c9332b5c89da453a3fc49d09787
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-03 22:37:05 +0530

    icons set

--- impact-proj/src/components/HeroSettings/HeroFeature.tsx
+++ impact-proj/src/components/HeroSettings/HeroFeature.tsx
@@ -1,38 +1,35 @@
 import { ReactElement } from "react";
-import {
-  Box,
-  SimpleGrid,
-  Icon,
-  Text,
-  Image,
-  Stack,
-  Flex,
-} from "@chakra-ui/react";
+import { Box, SimpleGrid, Text, Image, Stack, Flex } from "@chakra-ui/react";
 import { FcAssistant, FcDonate, FcInTransit } from "react-icons/fc";
 import "./heroFeature.css";
 import image1 from "../images/stack.png";
 import image2 from "../images/devices.png";
 import image3 from "../images/local_mall.png";
+import Icon from "./IconsFolder/Icon";
+import Icon2 from "./IconsFolder/Icon2";
+import Icon3 from "./IconsFolder/Icon3";
 
 interface FeatureProps {
   title: string;
   text: string;
-  icon: any;
+  iconName: any;
 }
 
-const Feature = ({ title, text, icon }: FeatureProps) => {
+const Feature = ({ title, text, iconName }: FeatureProps) => {
   return (
-    <Stack>
-      <Flex
-        w={16}
-        h={16}
-        align={"center"}
-        justify={"center"}
-        color={"white"}
-        rounded={"full"}
-        mb={1}>
-        {icon}
-      </Flex>
+    <Stack className="cardStack" direction={{ base: "column", lg: "row" }}>
+      <Stack>
+        <Flex
+          w={16}
+          h={16}
+          align={"flex-start"}
+          justify={"center"}
+          color={"white"}
+          mb={-6}
+          mr={3}>
+          {iconName}
+        </Flex>
+      </Stack>
       <Stack align="flex-start">
         <Text fontWeight={600} fontSize={"20px"} font-family={"Montserrat"}>
           {title}
@@ -55,21 +52,21 @@ export default function HeroFeature() {
       color={"white"}>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
         <Feature
-          icon={<Image src={image1} alt="image1" className="img" />}
+          iconName={<Icon2 icon="layers1" size={"50px"} color="white" />}
           title={"Design"}
           text={
             "Sapiente, assumenda suscipit autem ab expedita dignissimos, corrupti iusto nobis ex aperiam cumque reprehenderit, saepe culpa qui eos ullam minima quo, consequuntur."
           }
         />
         <Feature
-          icon={<Image src={image2} alt="image2" className="img" />}
+          iconName={<Icon3 icon="phonelink" size={"50px"} color="white" />}
           title={"Applications"}
           text={
             "Sapiente, assumenda suscipit autem ab expedita dignissimos, corrupti iusto nobis ex aperiam cumque reprehenderit, saepe culpa qui eos ullam minima quo, consequuntur."
           }
         />
         <Feature
-          icon={<Image src={image3} alt="image3" className="img" />}
+          iconName={<Icon icon="local_mall" size={"50px"} color="white" />}
           title={"e-commerce"}
           text={
             "Sapiente, assumenda suscipit autem ab expedita dignissimos, corrupti iusto nobis ex aperiam cumque reprehenderit, saepe culpa qui eos ullam minima quo, consequuntur."
