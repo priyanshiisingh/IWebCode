0685dbe7c6616d485c1a721716001911d4b82f0d
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-07 17:43:05 +0530

    body file made

--- impact-proj/src/App.tsx
+++ impact-proj/src/App.tsx
@@ -1,38 +1,17 @@
 import * as React from "react";
 import Navbar from "./components/NavbarSettings/Navbar";
 import { navigation } from "./components/NavbarSettings/Navigation";
-import Hero from "./components/HeroSettings/Hero";
-import WhyUsFeature from "./components/WhyUsSettings/WhyUs";
-import WhiteBg from "./components/WhiteBgSettings/WhiteBg";
-import VideoSec from "./components/VideoSecSettings/VideoSec";
-// import { Carousel } from "./components/CarouselSettings/Carousel";
-// import Testimonial from "./components/CarouselSettings/TestimonialSettings/Testimonial";
-import { Posts } from "./components/BlogPostSettings/Posts";
-import BlogPost from "./components/BlogPostSettings/BlogPost";
+import Body from "./Body";
 import Footer from "./components/FooterSettings/Footer";
 import "./App.css";
-import { SliderContent } from "./components/CarouselSettings/Slider";
-// import { New } from "./components/BlogPostSettings/New";
-import Modalex from "./components/VideoSecSettings/Modal";
 
 const App = () => {
   const { brand, links } = navigation;
   return (
     <div className="App">
-      {/* <Modalex /> */}
       <Navbar brand={brand} links={links} />
-      <Hero />
-      <WhyUsFeature />
-      <WhiteBg />
-      <VideoSec />
-
-      {/* <Carousel /> */}
-      <SliderContent />
-      {/* <Testimonial /> */}
-      <BlogPost />
-      <Posts />
+      <Body />
       <Footer />
-      {/* <New /> */}
     </div>
   );
 };
