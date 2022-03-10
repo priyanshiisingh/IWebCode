32017b3752537bd41b4adc9afd164b74a59d7c74
priyanshiisingh <priyanshianamika1815@gmail.com>
2022-03-10 09:54:53 +0530

    changes

--- my-app/src/components/HeroSettings/HeroLinks.tsx
+++
@@ -1,7 +0,0 @@
-const linking = {
-  people1: { src: "./src/images/persons-group-1.png", alt: "person1" },
-  content: { text: "Priyanshi" },
-  people2: { src: "./src/images/persons-group-2", alt: "people-2" },
-};
-
-export { linking };
